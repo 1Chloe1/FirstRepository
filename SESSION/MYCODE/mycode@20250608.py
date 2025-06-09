7af488c6b2960a38fd9b0364e638b4240cdfe51c
Hongwei <gmhwxi@gmail.com>
2025-06-08 20:51:10 -0400

    Updating: very very minorly

--- SESSION/MYCODE/mycode@20250608.py
+++ SESSION/MYCODE/mycode@20250608.py
@@ -75,3 +75,55 @@ def minusend1(xs):
       return cons(hd(xs), minusend1(tl(xs)))
 
 ##################################################################
+
+def last(xs):
+    if nilq(xs):
+        assert False
+    else:
+        if len(xs)==1:
+            return hd(xs)
+        else:
+            return last(tl(xs))
+
+xs = [1,2,3]
+print("last(" + str(xs) + ") = " + str(last(xs)))
+
+# def splitlist(xs,ys):
+#     if nilq(xs):
+#         return ([], [])
+#     else:
+#         if len(xs)==len(ys):
+#             return (xs,ys)
+#         else:
+#             if len(xs)==len(ys)+1:
+#                 return (xs,ys)
+#             else:
+#                 return splitlist(minusend(xs), cons(last(xs),ys))
+#
+# xs = [1,2,3,4,5,6,7,8,9,10,11]
+# print("splitlist(xs, []) = ", str(splitlist(xs, [])))
+
+def list_split(xs):
+    n2 = len(xs)//2
+    def helper(xs, ys):
+        if len(ys)==n2:
+            return (reverse(ys), xs)
+        else:
+            return helper(tl(xs), cons(hd(xs), ys))
+    return helper(xs, [])
+
+xs = [1,2,3,4,5,6,7,8,9,10,11]
+print("list_split(xs, []) = ", str(list_split(xs)))
+
+def list_altsplit(xs):
+    ln = len(xs)
+    if ln==0:
+        return ([], [])
+    elif ln==1:
+        return ([hd(xs)], [])
+    else:
+        (ys, zs) = list_altsplit(tl(tl(xs)))
+        return (cons(hd(xs), ys), cons(hd(tl(xs)), zs))
+
+xs = [1,2,3,4,5,6,7,8,9,10,11]
+print("list_altsplit(" + str(xs) + ") = ", str(list_altsplit(xs)))
